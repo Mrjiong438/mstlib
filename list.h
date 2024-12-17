002286bcdc08585cf7396c34e30f83bee3a8caf1
mrjiong <3268839661@qq.com>
2024-12-17 14:46:48 +0800

    keep doing list.h

--- list.h
+++ list.h
@@ -1,5 +1,9 @@
+#ifndef MST_LIST
+#define MST_LIST
+
 #include<stddef.h>
 #include<stdio.h>
+#include<stdlib.h>
 
 #define List(type)		\
 	struct{				\
@@ -7,24 +11,28 @@
 		size_t length;	\
 	}
 
-typedef struct{
-	void *item;
-	size_t length;
-}list;
+#define list_append(l,i)                                                     \
+	{                                                                        \
+		if(sizeof(*l.item) != sizeof(i)){                                    \
+			fprintf(stderr,"list_append:wrong type\n");                      \
+		}                                                                    \
+		if(l.length == 0){                                                   \
+			l.item = malloc(sizeof(*l.item));                                \
+			if(l.item != NULL)                                               \
+				l.length++;                                                  \
+			else                                                             \
+				perror("l.item malloc failed");                              \
+		}                                                                    \
+		else{                                                                \
+			void *temp_p = realloc(l.item,(l.length+1)*sizeof(*l.item));     \
+			if (temp_p != NULL){                                             \
+				l.item = temp_p;                                             \
+				l.length++;                                                  \
+			}                                                                \
+			else                                                             \
+				perror("l.item append realloc failed");                      \
+		}                                                                    \
+                                                                             \
+	}while(0)
 
-int list_append(list l,void *i){
-	if(sizeof(l.item) != sizeof(i)){
-		/* fprintf(stderr,"list_append:wrong type\n"); */
-		return 1;
-	}
-	return 0;
-}
-
-/* #define list_append(l,i)									\ */
-/* 	{																\ */
-/* 		if(sizeof(l.item) != sizeof(i)){							\ */
-/* 			/1* fprintf(stderr,"list_append:wrong type\n"); *1/		\ */
-/* 			return 1;												\ */
-/* 		}															\ */
-/* 		return 0;													\ */
-/* 	}while(0) */
+#endif
