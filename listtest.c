911d0108116498ccbb2e5a49cd50e62f7c1f4710
mrjiong <misjiong@gmail.com>
2024-12-17 14:46:48 +0800

    keep doing list.h

--- listtest.c
+++ listtest.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"list.h"
+/* #include"list.h" */
 
 int main(){
 	int b;
-	list(char) a={0};
-	if(a.item==NULL)
-		printf("null\n");
-	printf("%zu\n",a.length);
-	list_append(a,'i');
+	int *p=NULL;
+	printf("%zu\n",sizeof(*p));
+	/* list(char) a={0}; */
+	/* if(a.item==NULL) */
+	/* 	printf("null\n"); */
+	/* printf("%zu\n",a.length); */
+	/* list_append(a,'i'); */
 	return 0;
 }
