020c23b321b16c79a41e922fa520c0d3976fd237
mrjiong <misjiong@gmail.com>
2025-02-24 15:08:29 +0800

    arrange

--- example/listtest.c
+++ example/listtest.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"list.h"
+#include"../src/list.h"
 
 int main(){
 	List(long) a = {0};
@@ -11,11 +11,11 @@ int main(){
 	List_append(a,4);
 	List_append(a,5);
 
-	List_insert(a,1,6);
-	List_insert(a,1,7);
-	List_insert(a,1,8);
-	List_insert(a,1,9);
-	List_insert(a,1,10);
+	/* List_insert(a,1,6); */
+	/* List_insert(a,1,7); */
+	/* List_insert(a,1,8); */
+	/* List_insert(a,1,9); */
+	/* List_insert(a,1,10); */
 
 	List_pop(a);
 
@@ -23,6 +23,7 @@ int main(){
 		printf("%ld\n",a.item[i]);
 	}
 	printf("length:%zu\n",a.length);
+	printf("size:%zu\n",sizeof(a.item));
 
 	if(a.item == NULL){
 		printf("NULL\n");
