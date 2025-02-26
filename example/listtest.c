dfbff240e31ef279fb4b2fed71a18c2596a210d0
mrjiong <misjiong@gmail.com>
2025-02-26 17:35:05 +0800

    refactor list.h : now list only increace

--- example/listtest.c
+++ example/listtest.c
@@ -11,19 +11,19 @@ int main(){
 	List_append(a,4);
 	List_append(a,5);
 
-	/* List_insert(a,1,6); */
-	/* List_insert(a,1,7); */
-	/* List_insert(a,1,8); */
-	/* List_insert(a,1,9); */
-	/* List_insert(a,1,10); */
-
 	List_pop(a);
+
+	List_insert(a,1,6);
+	List_insert(a,1,7);
+	List_insert(a,1,8);
+	List_insert(a,1,9);
+	List_insert(a,1,10);
 
 	for(int i = 0;i < a.length;i++){
 		printf("%ld\n",a.item[i]);
 	}
 	printf("length:%zu\n",a.length);
-	printf("size:%zu\n",sizeof(a.item));
+	printf("size:%zu\n",a.size);
 
 	if(a.item == NULL){
 		printf("NULL\n");
