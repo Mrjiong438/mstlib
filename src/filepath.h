864a6599f6fad225e1b33fb5e7eec1ffc01f37f3
mrjiong <3268839661@qq.com>
2025-03-07 14:20:15 +0800

    add filepath : file_getname add split_num.h

--- src/filepath.h
+++ src/filepath.h
@@ -1,17 +1,26 @@
 #include<stdlib.h>
 #include<string.h>
 
-void get_filepath(char **pointer,const char *str){
+void file_getpath(char dest[],const char *str){
 	int i = (int)strlen(str)-1;
 	while(str[i] != '/'){
 		if(i==0)
 			break;
 		i--;
 	}
-	*pointer = (char*)malloc((i+1) * sizeof(char));
-	if (*pointer == NULL){
+	if (dest == NULL){
 		return;
 	}
-	strncpy(*pointer,str,i+1);
-	(*pointer)[i] = '\0';
+	strncpy(dest,str,i+1);
+	dest[i] = '\0';
+}
+
+char* file_getname(const char *str){
+	int i = (int)strlen(str)-1;
+	while(str[i] != '/'){
+		if(i==0)
+			break;
+		i--;
+	}
+	return (char *)(str + i + 1);
 }
