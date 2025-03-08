f00b2dcdd5a063c55069ad18a5bec81d88308355
mrjiong <misjiong@gmail.com>
2025-03-08 21:19:05 +0800

    add the macro "IMPLEMENTATION" listtext.c : now use the pointer (i think it's better

--- src/filepath.h
+++ src/filepath.h
@@ -1,9 +1,25 @@
+#ifndef FILE_PATH
+#define FILE_PATH
 #include<stdlib.h>
 #include<string.h>
 
+#ifdef _WIN32
+    /* #include<Windows.h> */
+    #include<direct.h>
+#endif//WIN32
+#ifdef __linux__
+    #include<sys/stat.h>
+    #include<sys/types.h>
+#endif
+
+void file_getpath(char dest[],const char *str);
+char* file_getname(const char *str);
+int makedir(const char *path);
+
+#ifdef FILE_PATH_IMPLEMENTATION
 void file_getpath(char dest[],const char *str){
 	int i = (int)strlen(str)-1;
-	while(str[i] != '/'){
+	while(str[i] == '\\' || str[i] != '/'){
 		if(i==0)
 			break;
 		i--;
@@ -16,11 +32,20 @@ void file_getpath(char dest[],const char *str){
 }
 
 char* file_getname(const char *str){
-	int i = (int)strlen(str)-1;
-	while(str[i] != '/'){
-		if(i==0)
-			break;
-		i--;
-	}
+	int i = (int)strlen(str) - 1;
+	for(;i > 0;i--)
+		if(str[i] == '\\' || str[i] == '/') break;
 	return (char *)(str + i + 1);
 }
+
+int makedir(const char *path){
+#ifdef _WIN32
+	return _mkdir(path);
+#endif//WIN32
+#ifdef __linux__
+	return mkdir(path, 0777);
+#endif//__linux__
+}
+
+#endif//FILE_PATH_IMPLEMENTATION
+#endif//FILE_PATH
