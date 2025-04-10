b46f1f749b503807af035beb2e70677bb7d85ec6
mrjiong <3268839661@qq.com>
2025-04-10 23:12:09 +0800

    strop.h: add mst_string_list_ull()

--- example/strop/filepath.c
+++ example/strop/filepath.c
@@ -4,8 +4,11 @@
 
 
 int main(int argc,const char *argv[]){
+	if (argc < 2) return 1;
 	size_t ptrlist[50];
-	char *str = "114 514 1919 810 asdasd \n";
+	/* char *str = "114,514,1919,810,asdasd \n"; */
+	char str[64];
+	strcpy(str,argv[1]);
 	char dest[64];
 	for(size_t i = 0;i < strlen(str);i++)
 		printf("%02lX ",i);
@@ -13,7 +16,10 @@ int main(int argc,const char *argv[]){
 	for(size_t i = 0;i < strlen(str);i++)
 		printf("%02X ",str[i]);
 	printf("\n");
-	size_t num = mst_string_split(ptrlist,50,dest,str,NULL);
+
+	size_t num = mst_string_split(ptrlist,50,dest,str,",");
+	printf("num:%zu\n",num);
+
 	/* str[0] = '\0'; */
 	for(size_t i = 0;i < strlen(str);i++)
 		printf("%02X ",dest[i]);
@@ -23,5 +29,7 @@ int main(int argc,const char *argv[]){
 	for(size_t i = 1;i < num;i++)
 		printf("%*s%02lX ",(int)(3*(ptrlist[i] - ptrlist[i-1] - 1)),"",ptrlist[i]);
 	printf("\n");
+
+	printf("%s\n",dest + ptrlist[0]);
 	return 0;
 }
