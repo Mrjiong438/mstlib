107c8a56b75b57e5672a72842972a40d51ba496e
mrjiong <3268839661@qq.com>
2025-04-14 09:22:07 +0800

    change the test

--- example/strop/numlist.c
+++ example/strop/numlist.c
@@ -5,17 +5,19 @@
 
 int main(int argc,const char *argv[]){
 	if (argc < 2) return 1;
-	char str[64];
+	char str[1024];
 	strcpy(str,argv[1]);
 	unsigned long long dest[64];
 
-	printf("%s\n",str);
+	/* printf("%s\n",str); */
 	size_t num = mst_string_list_ull(dest,50,str);
-	printf("num:%zu\n",num);
+	/* printf("num:%zu\n",num); */
 
 	for(size_t i = 0;i < num;i++){
 		printf("%lld,",dest[i]);
 	}
+	putc('\n',stdout);
+	fflush(stdout);
 
 	return 0;
 }
