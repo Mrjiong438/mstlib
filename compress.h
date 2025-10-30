ca66fac4044de0018efe78993ab1b617492a6273
mrjiong <misjiong@gmail.com>
2025-10-30 14:47:03 +0800

    wip

--- compress.h
+++ compress.h
@@ -2,11 +2,80 @@
 #define MST_COMPRESS
 #include<stdio.h>
 #include<string.h>
+//e_m2f means encode file to file
+//d_f2m means decode memery to memery
+//and so on
+//
+//get data and write least 9 byte most 17 byte to write_buf,return how many byte writed
+
+const size_t la_buff_size = 258;
 
 #ifdef MST_COMPRESS_IMPLEMENTATION
-void lzss(char *data,size_t len,FILE fp){
-    unsigned char write_buf[9];
-    write_buf[0] = 0x00;
+//is a weird implementation but basicly a changed dynamic programming way of longest common substring
+size_t longest_match(
+        const unsigned char *targit,const size_t tlen,
+        const unsigned char *parent,const size_t plen,
+        size_t *list
+    ){
+    max_index = 0;
+    max_length = 0;
+    for(size_t i = 0;i < tlen;i++){
+        if(targit[i] == parent[0]){
+            list[i % plen] = 1;
+            if(list[i % plen] > max_length){
+                max_length = list[i % plen];
+                max_index = i;
+            }
+        }
+        for(size_t j = 1;j < plen && i >= j;j++){
+            if(targit[i] != parent[j]){
+                list[(i-j) % plen] = 0;
+            }else
+            if(list[(i-j) % plen] != 0){
+                list[(i-j) % plen]++;
+            }
+
+            if(list[(i-j) % plen] > max_length){
+                max_length = list[(i-j) % plen];
+                max_index = i-j;
+            }
+        }
+    }
+    return max_index;
 }
+//return the size of compressed data
+size_t lzss_c_f2f_pro(
+        const char *in_file,const char *out_file,
+        unsigned char *sliding_buff,size_t window_size
+    ){
+    unsigned char write_buf[17];
+    size_t writed_byte_count = 0;
+    write_buf[0] = 0x00;
+    unsigned char flag_mask = 0x01;
+    for(size_t i = 1;i < len;){
+    }
+}
+/* size_t lzss_c_m2f_pro( */
+/*         const void *data,size_t len,const char *out_file, */
+/*         unsigned char *sliding_buff,size_t window_size */
+/*     ){ */
+/*     unsigned char write_buf[17]; */
+/*     size_t writed_byte_count = 0; */
+/*     write_buf[0] = 0x00; */
+/*     unsigned char flag_mask = 0x01; */
+/*     for(size_t i = 1;i < len;){ */
+/*     } */
+/* } */
+/* size_t lzss_c_m2m_origen( */
+/*         unsigned char *data,size_t len,unsigned char *out_buff, */
+/*         unsigned char *sliding_buff,size_t window_size */
+/*     ){ */
+/*     unsigned char write_buf[17]; */
+/*     size_t writed_byte_count = 0; */
+/*     write_buf[0] = 0x00; */
+/*     unsigned char flag_mask = 0x01; */
+/*     for(size_t i = 1;i < len;){ */
+/*     } */
+/* } */
 #endif//MST_COMPRESS_IMPLEMENTATION
 #endif//MST_COMPRESS
